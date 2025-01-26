a8d2ca37e1062bee37f83e888e4d0c0466129a4d
Samuel Jacobs <97236680+samueljacobs98@users.noreply.github.com>
2025-01-26 11:07:06 +0000

    Revert "feat: include metadata in PromptTemplate build result"
    This reverts commit f567f7195874d3da02ecad7df6f9ff2889641753.

--- test/test-d/index.test-d.ts
+++ test/test-d/index.test-d.ts
@@ -2,10 +2,7 @@ import { z } from "zod";
 import { expectNotType, expectType } from "tsd";
 
 import { promptTemplate } from "../../src/index";
-import {
-  PromptTemplateDataInput,
-  PromptTemplateDataOutput,
-} from "../../src/types";
+import { PromptTemplateInput, PromptTemplateOutput } from "../../src/types";
 
 const myTemplate = promptTemplate(
   z.object({
@@ -15,10 +12,10 @@ const myTemplate = promptTemplate(
   "Hello {{name}}, you are {{age}} years old!",
 );
 
-type MyTemplateInput = PromptTemplateDataInput<typeof myTemplate>;
+type MyTemplateInput = PromptTemplateInput<typeof myTemplate>;
 expectType<{ name: string; age: number }>(null as unknown as MyTemplateInput);
 
-type MyTemplateOutput = PromptTemplateDataOutput<typeof myTemplate>;
+type MyTemplateOutput = PromptTemplateOutput<typeof myTemplate>;
 expectType<{ name: string; age: number }>(null as unknown as MyTemplateOutput);
 
 expectNotType<{ name: string }>(null as unknown as MyTemplateInput);
